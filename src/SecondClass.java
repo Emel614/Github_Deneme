60ae1485dcf98651f50a6f3900f2a7129ac55433
Emel614 <85802220+Emel614@users.noreply.github.com>
2021-08-07 22:43:41 +0200

    Update SecondClass.java

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,5 +1,6 @@
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("My second class");
+        System.out.println("My second class is changed by Emel");
     }
 }
